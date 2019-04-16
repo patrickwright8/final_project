9b5d792cb5d1e699b9cbc16160420b27d1e73860
patrickwright8 <46975870+patrickwright8@users.noreply.github.com>
2019-04-16 15:56:59 -0400

    Updated csv reader
    This version has added the howbeauty array and has the beginnings of a set of loops to classify the data into arrays that will mesh nicely (no blank values while maintaining the large data set)

--- csvreader.py
+++ csvreader.py
@@ -5,7 +5,6 @@ Author: Patrick Wright
 
 import csv
 import numpy as np
-import time #Debug
 
 def readcsv():
     howhappy = np.array([]) # Array of blood glucose
@@ -14,12 +13,14 @@ def readcsv():
     age = np.array([])
     howsafe = np.array([])
     sex = np.array([])
+    howbeautiful = np.array([])
     with open('survey.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             howhappy = np.append(howhappy, row[2])
             howsatisfied = np.append(howsatisfied, row[3])
             howsafe = np.append(howsafe, row[22])
+            howbeautiful = np.append(howbeautiful, row[23])
             sex = np.append(sex, row[26])
             age = np.append(age, row[28])
             income = np.append(income, row[38])
@@ -29,15 +30,50 @@ def readcsv():
     sex = np.delete(sex, 0)
     age = np.delete(age, 0)
     income = np.delete(income, 0)
-    return howhappy, howsatisfied, howsafe, sex, age, income
+    howbeautiful = np.delete(howbeautiful, 0)
+    return howhappy, howsatisfied, howsafe, sex, age, income, howbeautiful
 
-howhappy, howsatisfied, howsafe, sex, age, income = readcsv()
+howhappy, howsatisfied, howsafe, sex, age, income, howbeautiful = readcsv()
+# Below creates a "classification" array whose value is 1 if x-array[index] AND y-array[index] both exist
+incomeandhappy = np.zeros(income)
+sexandsafe = np.zeros(howsafe)
+ageandhappy = np.zeros(age)
+incomeandbeauty = np.zeros(income)
+index = 0
+while index < len(income):
+    try:
+        
 
-for index in range(len(howhappy)-1):
-    try: 
+
+
+
+
+
+
+
+'''
+
+Correlations:
+    income (x axis) and happiness (y axis)
+    sex and how safe they feel (clustering where male and female is the classification and howsafe is x AND y axis
+    age (x axis) and happiness (y axis)
+    income (x axis) and satisfaction of beauty of neighborhood (y axis)
+    
+    
+    
+
+while index < len(howhappy):
+    try:
         howhappy[index] = float(howhappy[index])
+        howsatisfied[index] = float(howsatisfied[index])
+        howsafe[index] = float(howsafe[index])
+        index = index + 1
     except:
-        print(howhappy[index], "at", index, " could not be converted to a float. Deleting...")
         howhappy = np.delete(howhappy, index)
-        time.sleep(0.05)
-        index = index-1
+        howsatisfied = np.delete(howsatisfied, index)
+        howsafe = np.delete(howsafe, index)
+howhappy = howhappy.astype(np.float)
+howsatisfied = howsatisfied.astype(np.float)
+howsafe = howsafe.astype(np.float)
+
+'''

0fabcd3ef50a44dde1187e90ee59c5d7c2cdb275
patrickwright8 <46975870+patrickwright8@users.noreply.github.com>
2019-04-22 21:23:26 -0400

    Update csvreader.py

--- csvreader.py
+++ csvreader.py
@@ -7,13 +7,13 @@ import csv
 import numpy as np
 
 def readcsv():
-    howhappy = np.array([]) # Array of blood glucose
-    howsatisfied = np.array([]) # Array of hemoglobin
-    income = np.array([]) # Array of Class
-    age = np.array([])
-    howsafe = np.array([])
-    sex = np.array([])
-    howbeautiful = np.array([])
+    howhappy = np.array([], dtype=object) 
+    howsatisfied = np.array([],dtype=object) 
+    income = np.array([],dtype=object) 
+    age = np.array([],dtype=object)
+    howsafe = np.array([],dtype=object)
+    sex = np.array([],dtype=object)
+    howbeautiful = np.array([],dtype=object)
     with open('survey.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
@@ -33,35 +33,48 @@ def readcsv():
     howbeautiful = np.delete(howbeautiful, 0)
     return howhappy, howsatisfied, howsafe, sex, age, income, howbeautiful
 
+'''
+Even though these functions will just be imported to a driver, 
+if you read perform the functions in top-->bottom order, this should be
+where the cleaning functions are run. (because they have to be run before )
+'''
+
+
+
 howhappy, howsatisfied, howsafe, sex, age, income, howbeautiful = readcsv()
 # Below creates a "classification" array whose value is 1 if x-array[index] AND y-array[index] both exist
-incomeandhappy = np.zeros(income)
-sexandsafe = np.zeros(howsafe)
-ageandhappy = np.zeros(age)
-incomeandbeauty = np.zeros(income)
-index = 0
-while index < len(income):
-    try:
-        
+def createclassarrays(income, howsafe, age):
+    incomeandhappy = np.zeros(income)
+    sexandsafe = np.zeros(howsafe)
+    ageandhappy = np.zeros(age)
+    incomeandbeauty = np.zeros(income)
+    return incomeandhappy, sexandsafe, ageandhappy, incomeandbeauty
 
-
-
-
-
+def createclasses(array1, array2, classarray):
+    index = 0
+    while index < len(array1):
+        try:
+            array1[index] = float(array1[index])
+            array2[index] = float(array2[index])
+            classarray[index] = 1
+            index = index+1
+        except:
+            classarray[index] = 0
+            index = index+1
+    return classarray
 
 
 
 '''
-
+***Notes***
 Correlations:
     income (x axis) and happiness (y axis)
     sex and how safe they feel (clustering where male and female is the classification and howsafe is x AND y axis
     age (x axis) and happiness (y axis)
-    income (x axis) and satisfaction of beauty of neighborhood (y axis)
+    income (x axis) and how satisfied with beauty of neighborhood (y axis)
     
     
-    
-
+***Old Code***
 while index < len(howhappy):
     try:
         howhappy[index] = float(howhappy[index])
